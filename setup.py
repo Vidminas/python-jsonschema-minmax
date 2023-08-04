d41497498b380db8922b78991954858c6fbe9ab2
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2023-08-04 21:43:44 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name="jsonschema_minmax",
+    name="jsonschema-minmax",
     version="1.0",
     description="A Draft2020-21 metaschema and python-jsonschema extension to allow defining and validating relative min/max constraints",
     author="Vidminas Mikucionis",
     author_email="5411598+Vidminas@users.noreply.github.com",
     license="MIT",
     url="https://github.com/Vidminas/python-jsonschema-minmax",
-    packages=["src"],
+    py_modules=["src"],
     install_requires=[
         "referencing >= 0.30.0",
         "jsonschema >= 4.18.4",
