1927bd28d61cbaedcbd9406cbabaa4de5ed0d85e
Vidminas Mikucionis <5411598+Vidminas@users.noreply.github.com>
2023-08-05 09:50:16 +0100

    Ensure metaschemas are included in package

--- src/jsonschema_minmax/registry.py
+++ src/jsonschema_minmax/registry.py
@@ -1,20 +1,23 @@
+import pkgutil
 import json
-
-from pathlib import Path
 
 from referencing import Resource
 from referencing import Registry as _Registry
 from referencing.jsonschema import SchemaRegistry as _SchemaRegistry
 
 
-def load_metaschema(paths: list[Path]):
+def load_metaschema(paths: list[str]):
     for path in paths:
-        contents = json.loads(path.read_text(encoding="utf-8"))
+        data = pkgutil.get_data(__package__, path)
+        contents = json.loads(data.decode("utf-8"))
         yield Resource.from_contents(contents)
 
-schemas = load_metaschema([
-    Path("metaschema", "minmax-metaschema.json"),
-    Path("metaschema", "minmax-vocabulary.json"),
-])
+
+schemas = load_metaschema(
+    [
+        "metaschema/minmax-metaschema.json",
+        "metaschema/minmax-vocabulary.json",
+    ]
+)
 
 REGISTRY: _SchemaRegistry = (schemas @ _Registry()).crawl()
